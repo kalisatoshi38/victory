9a0458122d746d42900e683dfd64d4150638d911
kalisatoshi38 <74408877+kalisatoshi38@users.noreply.github.com>
2022-10-13 18:58:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ function hideMenu(){
     navLinks.style.right="-200px";
 }
 	
-	
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycby4F2BXOUGmFGeT-RfW9Jbv-I2C2fVM6BgmhwsAqYPwyxseLwWdnDDMbX0FN99c-FB5iQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg  = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+          msg.innerHTML="Message sent successfully"
+          setTimeout(function(){
+              msg.innerHTML=""
+          },5000)
+          form.reset()
+           
+      })
+      .catch(error => console.error('Error!', error.message))
+  });	
 	
 
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbxQ5VCCyNGjKPycXYkS0GaLMCZ6ajD8Ab7FXVW2SjW1a3ea10SzkU7m0_Ki7EcUBM9Hrg/exec'
-        const form = document.forms['submit-to-google-sheet']
-        const msg  = document.getElementById("mg")
-      
-      
-        form.addEventListener('submit', e => {
-          e.preventDefault()
-          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => {
-                mg.innerHTML="Message sent successfully"
-                setTimeout(function(){
-                    msg.innerHTML=""
-                },5000)
-                form.reset()
-                 
-            })
-            .catch(error => console.error('Error!', error.message))
-        })
+     
